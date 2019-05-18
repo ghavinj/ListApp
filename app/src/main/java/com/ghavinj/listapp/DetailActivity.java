54a54d37c81acf92742fe6501c382e7d75777347
GhavinJ <ghavinj@icloud.com>
2019-05-17 20:07:43 -0400

    took out scaleImg Method. fixed image sizes.g

--- app/src/main/java/com/ghavinj/listapp/DetailActivity.java
+++ app/src/main/java/com/ghavinj/listapp/DetailActivity.java
@@ -1,7 +1,13 @@
 package com.ghavinj.listapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,6 +16,45 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        Intent in = getIntent();
+        int index = in.getIntExtra("com.ghavinj.listapp.ITEM_INDEX", -1);
+
+        if (index > -1) {
+            int pic = getImg(index);
+            ImageView img = (ImageView) findViewById(R.id.imageView);
+            img.setImageResource(pic);
+        }
 
     }
-}
+    private int getImg(int index){
+           switch (index){
+               case 0: return R.drawable.peach;
+               case 1: return R.drawable.tomato;
+               case 2: return R.drawable.redgrapes;
+               default: return -1;
+           }
+    }
+
+    private void scaleImg(ImageView img, int pic){
+
+        Display screen = getWindowManager().getDefaultDisplay();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), pic, options);
+
+        int imgWidth = options.outWidth;
+        int screenWidth = screen.getWidth();
+
+        if (imgWidth > screenWidth){
+            int ratio = Math.round((float)imgWidth / (float)screenWidth);
+            options.inSampleSize = ratio;
+
+            options.inJustDecodeBounds = false;
+
+            Bitmap scaledImg = BitmapFactory.decodeResource(getResources(), pic, options);
+            img.setImageBitmap(scaledImg);
+        }
+
+        }
+  }
